b8aaa5ab4cf8b2a04158df61c398a841e1a0ec95
Simon Hoellerer <simon.hoellerer@bsse.ethz.ch>
2020-01-22 12:40:44 +0100

    update

--- NGS_data_analysis/run.sh
+++ NGS_data_analysis/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/hoelsimo/test_uASPIre/NGS_data_analysis/main_process_raw_NGS_data.sh /home/hoelsimo/test_uASPIre/uASPIre_RBS_example/config_uASPIre_RBS_example.cfg
+/home/hoelsimo/test_uASPIre/NGS_data_analysis/main_process_raw_NGS_data.sh /home/hoelsimo/test_uASPIre/uASPIre_RBS_example/config_uASPIre_example.cfg
 
